5e952880074ed4be33e9dcf368e3e016da030cb9
kal01aki <kal01aki@users.noreply.github.com>
2022-12-06 16:57:20 +0300

    Added methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        parkingLootK unityParkingLoot = new parkingLootK("unityParking", 7, 15);
+
+        unityParkingLoot.addVehicle(0, "Toyota Yaris", "128977","Medium",4);
+
+        unityParkingLoot.addVehicle(1, "Toyota Dx", "124545","Midium", 7);
+        unityParkingLoot.addVehicle(2, "Toyota Corolla", "126767","Large", 3);
+        unityParkingLoot.vehicleRemove(1);
+        unityParkingLoot.vehicleList();
+        unityParkingLoot.totalVehicleInTheParkingLoot();
+        unityParkingLoot.freeParkingLoot();
+
+
+//        unityParkingLoot.vehicleInfo(1);
+//
+//        System.out.println("the total amount of the the parking cost is:"+unityParkingLoot.vehicleParkingCost(2)+" Birr");
+
+
+
+
     }
+
+
+
+
 }
--- src/parkingLootK.java
+++ src/parkingLootK.java
@@ -1,2 +1,117 @@
-package PACKAGE_NAME;public class parkingLootK {
+import java.util.Arrays;
+
+public class parkingLootK {
+
+
+    int parkingLootCapacity;
+
+    String vehicleModelList[];
+    String vehiclePlateList[];
+    String carSizeList[];
+    int ParkingHourList[];
+    boolean PaymentStatusList[];
+    int parkingLootList[];
+    float chargePerHour;
+    parkingLootK(String name, int size, float perHour){
+
+        parkingLootCapacity = size;
+        chargePerHour = perHour;
+
+        vehicleModelList = new String[parkingLootCapacity];
+        vehiclePlateList = new String[parkingLootCapacity];
+        carSizeList = new String[parkingLootCapacity];
+        ParkingHourList = new int[parkingLootCapacity];
+        PaymentStatusList = new boolean[parkingLootCapacity];
+
+
+
+
+    }
+
+    String carPlateNumber;
+    String carSize;
+    String modelName;
+    int totalParkingHour;
+
+    boolean paymentStatus;
+    int parkingLootNumber;
+
+
+
+
+    public void addVehicle(int parkingLootNumber , String modelName,String carPlateNumber, String carSize, int totalParkingHour){
+
+        if(parkingLootCapacity <= totalVehicleInTheParkingLoot()){
+            System.out.println("All the parking loots are full we can't add the vehicle with a plate number"+carPlateNumber);
+        }
+        else {
+            vehicleModelList[parkingLootNumber] = modelName;
+            vehiclePlateList[parkingLootNumber] = carPlateNumber;
+            carSizeList[parkingLootNumber] = carSize;
+            ParkingHourList[parkingLootNumber] = totalParkingHour;
+
+            System.out.println("Successfully added the vehicle with the plat number of "+carPlateNumber);
+        }
+
+    }
+
+    public void vehicleInfo(int parkingLootNumber){
+        System.out.println("the model of the vehicle is "+vehicleModelList[parkingLootNumber]);
+        System.out.println("the plate Number of the vehicle is "+vehiclePlateList[parkingLootNumber]);
+        System.out.println("the size of  the vehicle is "+carSizeList[parkingLootNumber]);
+    }
+
+    public float vehicleParkingCost(int parkingLootNumber){
+        float payment = chargePerHour*ParkingHourList[parkingLootNumber];
+
+        return payment;
+    }
+
+    public void vehicleRemove(int parkingLootNumber){
+
+        vehiclePlateList[parkingLootNumber] = null;
+
+
+
+        System.out.println("The vehicle on parking loot number " + parkingLootNumber + " is discharged.");
+
+    }
+
+    public void vehicleList(){
+
+        System.out.println("The list of car's Plate No. that are currently parked on the loot are " + Arrays.toString(vehiclePlateList));
+
+
+    }
+    public int freeParkingLoot(){
+
+//        int currentSpace = 0;
+//
+//for (int i = 0; i < parkingLootList.length; i++) {
+//
+//
+//            if (parkingLootCapacity > parkingLootList){
+//
+//            currentSpace = parkingLootCapacity - parkingLootList;
+//
+//            return System.out.println(currentSpace);}
+//
+//            else
+        return 0;
+
+    }
+
+    public  int totalVehicleInTheParkingLoot(){
+        int count = 0;
+        for (String i:vehiclePlateList) {
+
+            if (i!=null){
+                count++;
+            }
+
+        }
+        return count;
+    }
+
+
 }
